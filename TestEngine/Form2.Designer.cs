1c18068db0a04629151d455d249a6b5336e97f95
X_huihui <64721484+xiaohuihui1022@users.noreply.github.com>
2022-10-07 18:31:50 +0800

    attackupdate

--- TestEngine/Form2.Designer.cs
+++ TestEngine/Form2.Designer.cs
@@ -31,8 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.sans = new System.Windows.Forms.PictureBox();
             this.heart = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.Line = new System.Windows.Forms.PictureBox();
+            this.HealthCount = new System.Windows.Forms.Label();
+            this.nowhealth = new System.Windows.Forms.Label();
+            this.dead = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.sans)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.heart)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Line)).BeginInit();
@@ -40,41 +42,64 @@
             // 
             // sans
             // 
-            this.sans.Location = new System.Drawing.Point(275, 12);
+            this.sans.Location = new System.Drawing.Point(275, 177);
             this.sans.Name = "sans";
-            this.sans.Size = new System.Drawing.Size(261, 230);
+            this.sans.Size = new System.Drawing.Size(261, 191);
             this.sans.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.sans.TabIndex = 0;
             this.sans.TabStop = false;
             // 
             // heart
             // 
-            this.heart.Location = new System.Drawing.Point(386, 379);
+            this.heart.BackColor = System.Drawing.Color.Transparent;
+            this.heart.Location = new System.Drawing.Point(387, 386);
             this.heart.Name = "heart";
-            this.heart.Size = new System.Drawing.Size(23, 24);
+            this.heart.Size = new System.Drawing.Size(16, 16);
             this.heart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.heart.TabIndex = 1;
             this.heart.TabStop = false;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(713, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 22);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
             // Line
             // 
             this.Line.Image = ((System.Drawing.Image)(resources.GetObject("Line.Image")));
-            this.Line.Location = new System.Drawing.Point(253, 273);
+            this.Line.Location = new System.Drawing.Point(275, 239);
             this.Line.Name = "Line";
-            this.Line.Size = new System.Drawing.Size(300, 253);
+            this.Line.Size = new System.Drawing.Size(261, 217);
             this.Line.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Line.TabIndex = 3;
             this.Line.TabStop = false;
+            // 
+            // HealthCount
+            // 
+            this.HealthCount.AutoSize = true;
+            this.HealthCount.ForeColor = System.Drawing.Color.Red;
+            this.HealthCount.Location = new System.Drawing.Point(85, 494);
+            this.HealthCount.Name = "HealthCount";
+            this.HealthCount.Size = new System.Drawing.Size(78, 22);
+            this.HealthCount.TabIndex = 5;
+            this.HealthCount.Text = "100/100";
+            // 
+            // nowhealth
+            // 
+            this.nowhealth.AutoSize = true;
+            this.nowhealth.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.nowhealth.Location = new System.Drawing.Point(169, 494);
+            this.nowhealth.Name = "nowhealth";
+            this.nowhealth.Size = new System.Drawing.Size(40, 22);
+            this.nowhealth.TabIndex = 6;
+            this.nowhealth.Text = "100";
+            this.nowhealth.Visible = false;
+            // 
+            // dead
+            // 
+            this.dead.AutoSize = true;
+            this.dead.Font = new System.Drawing.Font("微软雅黑", 42F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dead.ForeColor = System.Drawing.Color.Black;
+            this.dead.Location = new System.Drawing.Point(160, 261);
+            this.dead.Name = "dead";
+            this.dead.Size = new System.Drawing.Size(519, 150);
+            this.dead.TabIndex = 7;
+            this.dead.Text = "U are DEAD!\r\npress X to restart\r\n";
             // 
             // Form2
             // 
@@ -82,10 +107,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(784, 562);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.heart);
             this.Controls.Add(this.sans);
+            this.Controls.Add(this.HealthCount);
+            this.Controls.Add(this.heart);
             this.Controls.Add(this.Line);
+            this.Controls.Add(this.nowhealth);
+            this.Controls.Add(this.dead);
             this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "Form2";
@@ -103,7 +130,9 @@
 
         private System.Windows.Forms.PictureBox sans;
         private System.Windows.Forms.PictureBox heart;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox Line;
+        private System.Windows.Forms.Label HealthCount;
+        private System.Windows.Forms.Label nowhealth;
+        private System.Windows.Forms.Label dead;
     }
 }
--- TestEngine/Form2.cs
+++ TestEngine/Form2.cs
@@ -1,5 +1,9 @@
 ﻿using TheWorstEngine.AnimFunction;
+using TheWorstEngine.UIFunction;
 using System.Windows.Forms;
+using System.Threading;
+using System;
+using System.Drawing;
 
 namespace TestEngine
 {
@@ -8,11 +12,16 @@ namespace TestEngine
         TweAnim sans1 = new TweAnim();
         TweAnim Heart = new TweAnim();
         TweSound mega = new TweSound();
+        TweText Health = new TweText();
+        TweSound EndingSound = new TweSound();
+        private Thread Ending;
         public Form2()
         {
+            CheckForIllegalCrossThreadCalls = false;
             InitializeComponent();
             SansInit();
             HeartInit();
+            HealthInit();
             mega.Load(@".\sound\mega.ogg");
             mega.SoundPlay();
         }
@@ -21,7 +30,7 @@ namespace TestEngine
             Heart.Load(this, heart);
             Heart.SetResolution(new System.Drawing.Size(800, 600));
             Heart.SetImage(@".\img\red.png");
-            Heart.Encircle(Line, heart);
+            Heart.Encircle(Line, heart, 4, 19);
             Heart.KeySet(Keys.Up, Keys.Down, Keys.Left, Keys.Right);
             Heart.CanMove(true);
         }
@@ -31,12 +40,41 @@ namespace TestEngine
             string[] FS = { ".\\img\\sans1.png", ".\\img\\sans2.png",
                   ".\\img\\sans3.png", ".\\img\\sans4.png",
             ".\\img\\sans3.png", ".\\img\\sans2.png", ".\\img\\sans1.png"};
-            sans1.SetAnim(FS, 5, label1);
+            sans1.SetAnim(FS, 5);
+        }
+        private void HealthInit()
+        {
+            Health.Load(this, HealthCount, nowhealth);
+            Health.AttackCheck(sans, heart, 10, true, 500);
+            Health.SetHurtSound(@".\sound\uts\hurt.wav");
+            Ending = new Thread(EndThread);
+            Ending.Start();
         }
         private void Form2_FormClosing(object sender, FormClosingEventArgs e)
         {
             sans1.isClosed = true;
             mega.SoundStop();
+            Ending.Abort();
+        }
+        private void EndThread()
+        {
+            while (true)
+            {
+                if (nowhealth.Text == "0")
+                {
+                    mega.SoundStop();
+                    dead.BackColor = Color.Red;
+                    sans.Visible = false;
+                    heart.Visible = false;
+                    Line.Visible = false;
+                    HealthCount.Visible = false;
+                    EndingSound.Load(@".\sound\uts\afterdead.wav");
+                    EndingSound.SoundPlay();
+                    // TODO
+                    Ending.Abort();
+                }
+                Thread.Sleep(10);
+            }
         }
     }
 }
